6256359f20737c4942ba0508ca2a0e132c4eb472
mprojet90 <mprojet90@gmail.com>
2025-07-10 19:01:05 +0700

    Create route.js

---
+++ backend/api/lib/app/auth/callback/route.js
@@ -0,0 +1,22 @@
+// app/auth/callback/route.js
+
+import { createClient } from '@/lib/supabase/server'
+import { NextResponse } from 'next/server'
+
+export async function GET(request) {
+  const { searchParams, origin } = new URL(request.url)
+  const code = searchParams.get('code')
+  // jika 'next' ada di params, gunakan itu untuk redirect
+  const next = searchParams.get('next') ?? '/dashboard'
+
+  if (code) {
+    const supabase = createClient()
+    const { error } = await supabase.auth.exchangeCodeForSession(code)
+    if (!error) {
+      return NextResponse.redirect(`${origin}${next}`)
+    }
+  }
+
+  // URL untuk redirect jika terjadi error atau tidak ada kode
+  return NextResponse.redirect(`${origin}/login?error=Could not authenticate user`)
+}
